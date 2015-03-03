cf0ffd7f9c90120d4190c0e3aa0a71c1e6e7c97f
Rostislav Kirillov <rast@skbkontur.ru>
2015-03-03 17:34:31 +0500

    added nuspec and commands to build a nuget package

--- Experiments/Properties/AssemblyInfo.cs
+++ Experiments/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Experiments")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("an experimental project to build nuget package with travis-ci and github")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ACME Inc.")]
 [assembly: AssemblyProduct("Experiments")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
